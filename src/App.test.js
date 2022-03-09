bda4b61bcc2ab618c52e44bdc620ab35faf4d26f
Anushri <anushriagrawal90@gmail.com>
2022-03-10 00:07:41 +0530

    test correction

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Assignment/i);
+  const linkElement = screen.getByText(/Project/i);
   expect(linkElement).toBeInTheDocument();
 });
